355b41c9da5b57f0d56af7495855b4606b579982
FF <>
2024-05-19 04:13:14 +0200

    cleanup

--- py_metrics/fs.py
+++ py_metrics/fs.py
@@ -6,12 +6,6 @@
 
 import datetime, csv, sys, argparse, shutil, os
 
-parser = argparse.ArgumentParser(description="Write filesystem metrics into csv file")
-parser.add_argument('mounts', metavar="fs", type=str, nargs='*', help="mounts")
-parser.add_argument('--csv', help="output file", metavar="fs.csv")
-
-args = parser.parse_args()
-
 def now() -> str:
     return datetime.datetime.now().astimezone().replace(microsecond=0).isoformat()
 
@@ -21,28 +15,34 @@ def getMounts() -> list[str]:
     with open('/proc/mounts','r') as f:
         return [line.split()[1] for line in f.readlines()]
 
-fallback = False
-mounts = args.mounts
-f = sys.stdout
+def main():
+    parser = argparse.ArgumentParser(description="Write filesystem metrics into csv file")
+    parser.add_argument('mounts', metavar="fs", type=str, nargs='*', help="mounts")
+    parser.add_argument('--csv', help="output file", metavar="fs.csv")
+    args = parser.parse_args()
+    
+    fallback = False
+    mounts = args.mounts
+    f = sys.stdout
 
-if args.csv:
-    f = open(args.csv, 'w')
+    if args.csv:
+        f = open(args.csv, 'w')
 
-if len(mounts) == 0:
-    fallback = True
-    mounts = getMounts()
+    if len(mounts) == 0:
+        fallback = True
+        mounts = getMounts()
 
-data = list()
+    data = list()
 
-for m in mounts:
-    total, used, free = shutil.disk_usage(m)
-    if fallback and (total == 0 or used == 0): continue
-    data.append({"now.iso": now(), "fs": m, "total":total, "used":used, "free":free})
+    for m in mounts:
+        total, used, free = shutil.disk_usage(m)
+        if fallback and (total == 0 or used == 0): continue
+        data.append({"now.iso": now(), "fs": m, "total":total, "used":used, "free":free})
 
-if len(data) > 0:
-    w = csv.DictWriter(f, data[0].keys())
-    w.writeheader()
-    w.writerows(data)
+    if len(data) > 0:
+        w = csv.DictWriter(f, data[0].keys())
+        w.writeheader()
+        w.writerows(data)
 
-def poetry():
-    return True
+if __name__ == '__main__':
+    main()
